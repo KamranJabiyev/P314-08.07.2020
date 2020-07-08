1de59ff2df1220723e49a65603acf792de448ca3
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-07-08 21:45:37 +0400

    Add files via upload

---
+++ 08.07.2020 JS Intro/js/script.js
@@ -0,0 +1,142 @@
+"use strict"
+// var a=5;
+// a="Salam";
+// a=true;
+// console.log(a)
+
+// var a;
+// var name=null
+// console.log(name)
+
+// var a="a10";
+// var b=parseInt(a);
+// console.log(b)
+
+// var name="Kamran"
+// console.log(name/2);
+
+// let a=10;
+// console.log(a)
+
+// var x=10;
+
+// x=100;
+
+// var x=1000;
+
+// console.log(x)
+// let i=4;
+// let x=10;
+// let x=1000;
+// x=1000;
+// console.log(x)
+// for (let i = 0; i < 10; i++) {
+
+// }
+// console.log(i)
+
+// const pi = 3.14;
+// pi=1;
+// console.log(pi)
+// let a=10;
+// a=100;
+
+// const x=10;
+
+// x=12;
+// var x;
+// x = 10;
+// console.log(x)
+
+// let i=10
+// console.log(++i)
+
+// let i=10
+// i=11
+// let x=4;
+// let y=5;
+// x+=y; //x=x+y
+// console.log(x)
+
+// let name="Kamran"
+// let surname="Jabiyev"
+
+// console.log(name+" "+surname)
+
+// console.log(`${name} ${surname}`)
+
+// let x = '10';
+// let y = 1
+
+// console.log(parseInt(x) + y)
+
+// let x='10'
+// let y=10
+
+// console.log(x===y)
+
+// let age = 15;
+
+// let result = age > 16 ? "okey" : "Yashin chatmir"
+
+// console.log(result)
+
+// if(age>16)
+//     console.log("okey");
+// else if(age==16)
+//     console.log("16")
+// else
+//     console.log("Yashin chatmir")
+
+// switch (age) {
+//     case 18:
+//         console.log(18)
+//         break;
+//     case 17:
+//          console.log(17)
+//         break;
+//     default:
+//         console.log("hech biri deyil")
+//         break;
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// let i = 1;
+
+// while(i<10){
+
+//     console.log(i)
+//     i++;
+// }
+
+// do {
+//     i++;
+//     console.log(i)
+// } while (i < 10)
+
+// let arr=[1,2,3,4,5,true,"Kamran",'a',1.2]
+
+// console.log(arr)
+
+// let arr = [10, 20, 30];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// for (const item of arr) {
+//     console.log(item)
+// }
+
+let obj={
+    name:"Kamran",
+    surname:"Jabiyev",
+    age:30
+}
+
+for (const key in obj) {
+    console.log(`${key} -  ${obj[key]}`)
+}
+
